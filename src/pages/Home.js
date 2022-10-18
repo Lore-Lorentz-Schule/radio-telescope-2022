fef7b618066da1630e426638654d0ee5d7c4d850
AAWA-byt <mail@aaronwagner.earth>
2022-10-18 17:02:00 +0200

    :sparkles: imported design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,21 @@ import '../style/App.css';
 function Home() {
   return (
     <div className='App-header-Home'>
-        <h1>Title</h1>
-        <p><strong>Content</strong></p>
+        <div className='App__text'>
+          <div className='App__text_subtitle'>
+          <p>Projekt:</p> 
+          </div>
+          <h1>Radio Telescope 2022</h1>
+          <p><strong>Content</strong><br></br>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam 
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est 
+          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero 
+          eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus 
+          est Lorem ipsum dolor sit amet.</p>
+          </div>
+        <input type="submit" value="Submit" />
     </div>
   );
 }
