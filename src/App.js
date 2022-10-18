1829cb8b1a59a80c6317fc25c35f01d1ac47eecd
AAWA-byt <mail@aaronwagner.earth>
2022-10-18 11:18:26 +0200

    :sparkles: added router system

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './style/App.css';
-import About from './components/about_header';
-import Header from './components/app_header';
+import "./style/App.css";
+import React from 'react';
+
+// importing react-router-dom components
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+//importing components pages
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <About />
-    </div>  );
+    <Router> 
+    <Routes>
+      <Route path="/"  exact element={<Home />} />
+    </Routes>
+  </Router>
+  );
 }
 
 export default App;
