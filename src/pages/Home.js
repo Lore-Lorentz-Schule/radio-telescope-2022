24b1b7f8f7aeb95f0e1ce6719f4ebc6e056bba26
AAWA-byt <mail@aaronwagner.earth>
2022-10-18 22:50:29 +0200

    :sparkles: used new design for seccond "page" and added little "footer" #3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,46 @@
 import '../style/App.css';
+import lolo from '../images/lolo.png';
 
 function Home() {
   return (
-    <div className='App-header-Home'>
-        <div className='App__text'>
-          <div className='App__text_subtitle'>
-          <p>Projekt:</p> 
+    // App
+      <div className="App">
+        <div className='App-header-Home'>
+          <div className='App__text'>
+            <div className='App__text_subtitle'>
+              <p>Projekt:</p>
+            </div>
+            <h1>Radio Telescope 2022</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+              et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+              eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+              est Lorem ipsum dolor sit amet.</p>
+            <p><a href='/topics'><strong>Let's get started</strong></a></p>
           </div>
-          <h1>Radio Telescope 2022</h1>
-          <p><strong>Content</strong><br></br>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam 
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est 
-          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero 
-          eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus 
-          est Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className='App-header-About'>
+          <div className='App__text-about'>
+            <p>Dieses Projekt wurde von Schülern der
+              Klasse 12NP der Lore Lorentz Schule in
+              der  Projektwoche vom 21. November 2022
+              bis zum 26. November 2022 durchgeführt.</p>
+            <a href='http://www.lore-lorentz-schule.de'><img src={lolo} alt='lolo' className='App__lolo' /></a>
           </div>
-        <input type="submit" value="Submit" />
-    </div>
+
+          <div className='copyright'>
+           <p>© 2022 - Aaron Wagner<br></br>
+              Developed by <a href='https://www.aaronwagner.earth'>Aaron Wagner</a><br></br>
+              Source code on <a href='https://github.com/Lore-Lorentz-Schule/radio-telescope-2022'>GitHub</a><br></br>
+              <b>Contact:</b> <a href='mailto:webmaster@aaronwagner.earth'>webmaster@aaronwagner.earth</a></p>
+          </div>
+        </div>
+
+      </div>
   );
 }
 
