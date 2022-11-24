1bf2a72e9dd6f9d67317b78cf960a5c30c87c48f
AAWA-byt <mail@aaronwagner.earth>
2022-11-24 17:19:23 +0100

    :zap: added simple react return function

---
+++ src/compenents/ContentTable.js
@@ -0,0 +1,14 @@
+import '../style/ContentTable-Style.css';
+
+function ContentTable() {
+  return (
+    // App
+      <div className="App">
+        <div className="App-header-content">
+            <h1>Inhaltsverzeichnis</h1>
+        </div>
+      </div>
+  );
+}
+
+export default ContentTable;
