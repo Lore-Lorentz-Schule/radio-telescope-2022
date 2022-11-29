17fc483370232269807ce82480e92d09585dd789
AAWA-byt <mail@aaronwagner.earth>
2022-11-29 22:43:22 +0100

    :zap: added buttons for "Inhaltsverzeichnis"

--- src/compenents/ContentTable.js
+++ src/compenents/ContentTable.js
@@ -6,6 +6,11 @@ function ContentTable() {
       <div className="App">
         <div className="App-header-content">
             <h1>Inhaltsverzeichnis</h1>
+            <button id='about' type='submit' name='about'>Über dieses Projekt</button>
+            <button id='project' type='submit' name='project'>Projektplanung</button>
+            <button id='hardware' type='submit' name='hardware'>Hardware</button>
+            <button id='software' type='submit' name='software'>Software</button>
+
         </div>
       </div>
   );
